ae1ae4deb5f98a59d35910b2a269d9331bad8949
Mark Goho <markgoho@gmail.com>
2017-10-05 15:40:12 -0400

    Adding store setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
+import { Store } from '@ngrx/store';
+import { Post } from './models/post.model';
+import * as PostActions from './actions/post.actions';
+
+interface AppState {
+  post: Post;
+}
 @Component({
   selector: 'app-root',
+  styles: [''],
   template: `
-    <p>
-      app Works!
-    </p>
-  `,
-  styles: []
+    <div *ngIf="post$ | async as p">
+      <h2>{{ p.text }}</h2>
+      <h4>Votes: {{ p.likes }}</h4>
+      <button (click)="upvote()">Upvote</button>
+      <button (click)="downvote()">Downvote</button>
+      <button (click)="resetPost()">Reset</button>
+      <input [(ngModel)]="text">
+      <button (click)="editText()" >Change Title</button>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  post$: Observable<Post>;
+
+  text: string;
+  constructor(private store: Store<AppState>) {
+    this.post$ = this.store.select('post');
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
+import { postReducer } from './reducers/post.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ message: simpleReducer, post: postReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 10 })
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
